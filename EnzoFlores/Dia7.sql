7d11ab9d3068fef49f0ecba2ec19d638b9154c86
zoen34palabras <enzo.umaguma@gmail.com>
2024-04-23 20:37:49 -0400

    Creación tabla ventas

--- EnzoFlores/Dia7.sql
+++ EnzoFlores/Dia7.sql
@@ -10,3 +10,11 @@ varchar(25), año varchar(25), color varchar(25), precio float);
 entero y restriccion de primary key, marca es un dato de tipo caracteres y maximo de 25, modelo también es
 dato  de catacteres con maximo de 25 al igual que año y color. El campo precio tiene un dato float decimal */
 
+create table ventas(id serial unique not null, fecha varchar(20),
+id_auto int, cliente varchar(25), referencia int, cantidad float,
+metodo_pago varchar(10), foreign key (id_auto) references autos(id));
+/* Creamos la tabla llamada ventas con los campos id, fecha, id_auto, cliente, referencia, cantidad, metodo_pago. Id tiene un
+dato serial autoincrementabale, debe ser unico y no puede ser nulo, fecha es un dato de tipo caracteres y maximo de 20, 
+id_auto tiene un dato entero, cliente tien un dato de tipo caracteres y maximo de 25, referencia tiene un dato entero, cantidad
+tiene un dato float decimal, metodo_pago es de tipo caracteres y maximo de 10. La ultima linea define una relacion con una foreign
+key (columna id_auto) entre las tablas ventas y autos.*/
