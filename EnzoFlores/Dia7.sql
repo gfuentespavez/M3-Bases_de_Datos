5d2281968a9f45efa75641b134b7838e76bc30db
zoen34palabras <enzo.umaguma@gmail.com>
2024-04-23 20:05:32 -0400

    Creación tabla autos

--- EnzoFlores/Dia7.sql
+++ EnzoFlores/Dia7.sql
@@ -4,3 +4,9 @@ create database mawashi_cars;
 \c mawashi_cars;
 /* nos conectamos a la base de datos mawashi_cars */
 
+create table autos(id int primary key, marca varchar(25), modelo
+varchar(25), año varchar(25), color varchar(25), precio float);
+/* Creamos la tabla llamada autos con los campos id, marca, modelo, año, color y precio. Id tiene un dato
+entero y restriccion de primary key, marca es un dato de tipo caracteres y maximo de 25, modelo también es
+dato  de catacteres con maximo de 25 al igual que año y color. El campo precio tiene un dato float decimal */
+
