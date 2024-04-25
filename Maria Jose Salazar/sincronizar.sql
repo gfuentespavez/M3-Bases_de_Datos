311950c17d14f88b6ae26ca258af26c68b1dd3ea
Maria Jose <ma.josesalazaralfonzo@gmail.com>
2024-04-24 22:55:54 -0400

    actividad

--- Maria Jose Salazar/sincronizar.sql
+++ Maria Jose Salazar/sincronizar.sql
@@ -1,1 +1,25 @@
---buenas noches
+Crear una base de datos llamada películas.
+CREATE DATABASE Peliculas;
+
+--nos conectamos a la base de dato 
+\c Peliculas
+
+Cargar ambos archivos a su tabla correspondiente.
+CREATE TABLE peliculas
+
+CREATE TABLE reparto
+
+3. Obtener el ID de la película “Titanic”.
+(1 Punto)
+4. Listar a todos los actores que aparecen en la película "Titanic".
+(1 Punto)
+5. Consultar en cuántas películas del top 100 participa Harrison Ford.
+(2 Puntos)
+6. Indicar las películas estrenadas entre los años 1990 y 1999 ordenadas por título de
+manera ascendente.
+(1 punto)
+7. Hacer una consulta SQL que muestre los títulos con su longitud, la longitud debe ser
+nombrado para la consulta como “longitud_titulo”.
+(1 punto)
+8. Consultar cual es la longitud más grande entre todos los títulos de las películas.
+(2 punto)
